75ffeee7eb023f079aeb97c4c6aa1c426023f5c3
Sebastian Posadas <posadas.sca@gmail.com>
2019-08-05 01:51:17 -0300

    Agregando seguridad a la aplicacion con Spring Security.

---
+++ src/main/java/com/ejemplo/demo/converter/TestCrypt.java
@@ -0,0 +1,11 @@
+package com.ejemplo.demo.converter;
+
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+
+public class TestCrypt {
+
+    public static void main(String[] args) {
+        BCryptPasswordEncoder pe = new BCryptPasswordEncoder();
+        System.out.println(pe.encode("user"));
+    }
+}
---
+++ src/main/java/com/ejemplo/demo/entity/User.java
@@ -0,0 +1,73 @@
+package com.ejemplo.demo.entity;
+
+import org.springframework.security.core.GrantedAuthority;
+
+import javax.persistence.*;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
+
+@Entity
+public class User {
+
+    @Id
+    @Column(name = "username", unique = true, nullable = false, length = 45)
+    private String username;
+
+    @Column(name = "password", nullable = false, length = 60)
+    private String password;
+
+    @Column(name = "enabled", nullable = false)
+    private boolean enabled;
+
+    @OneToMany(fetch = FetchType.EAGER, mappedBy = "user")
+    private Set<UserRole> userRole = new HashSet<>();
+
+    public User() {
+    }
+
+    public User(String username, String password, boolean enabled, boolean b, boolean b1, boolean b2, List<GrantedAuthority> authorities) {
+        this.username = username;
+        this.password = password;
+        this.enabled = enabled;
+    }
+
+    public User(String username, String password, boolean enabled, Set<UserRole> userRole) {
+        this.username = username;
+        this.password = password;
+        this.enabled = enabled;
+        this.userRole = userRole;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public boolean isEnabled() {
+        return enabled;
+    }
+
+    public void setEnabled(boolean enabled) {
+        this.enabled = enabled;
+    }
+
+    public Set<UserRole> getUserRole() {
+        return userRole;
+    }
+
+    public void setUserRole(Set<UserRole> userRole) {
+        this.userRole = userRole;
+    }
+}
